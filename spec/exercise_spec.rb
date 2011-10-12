188b9b9c1e0661ad911a7a64271264cf81bad9f8
Jason Shaw <shawjaso@uw.edu>
2011-10-11 19:19:35 -0700

    Updating with all tests.

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -4,11 +4,41 @@ describe "Strings" do
 
     it "should remove all whitespace from the beginning and the end of the string" do
 
-        lyric = "  Hello, is it me you're looking for I can see it in your eyes  "
+        lyrics = "  Hello, is it me you're looking for I can see it in your eyes  "
 
-        lyrics.strip.should eq "Hello, is it me you're looking for I can see it in your eyes"
+        lyrics.strip.capitalize.should eq "Hello, is it me you're looking for I can see it in your eyes"
+
     end
 
   end
 
+end
+
+describe "Integer" do
+  
+  context "when calling even?" do
+    
+    it "should be false" do
+
+      1.even?.should eq true
+
+    end
+
+  end
+
+end
+
+describe "Symbol" do
+
+  context "when calling empty?" do
+
+    it "should be false" do
+
+      someSymbol = :"An Object"
+      someSymbol.empty?.should eq true
+
+    end
+
+  end
+  
 end
